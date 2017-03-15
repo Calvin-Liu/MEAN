9f420fa59c92c4cc01ef236ea1d0f4dde15cdeee
Calvin <calvinliu411@gmail.com>
2017-03-14 23:10:22 -0700

    Creating the Angular Application

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
-var express = require('express');
+var express = require('express'),
+	stylus = require('stylus'),
+	logger = require('morgan'),
+	bodyParser = require('body-parser');
+
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var app = express();
+var app = express(); 
+
+function compile(str, path) {
+	return stylus(str).set('filename', path);
+}
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(stylus.middleware(
+	{
+		src: __dirname + '/public',
+		compile: compile
+	}
+));
+app.use(express.static(__dirname + '/public')); //static route handling
+
+app.get('/partials/:partialPath', function(req, res) {
+	res.render('partials/' + req.params.partialPath);
+})
+
 app.get('*', function(req, res) {
 	res.render('index');
 })
