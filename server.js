b2f1ed3e127f85bf47012978515c32c0cf361e51
Calvin <calvinliu411@gmail.com>
2017-03-18 18:33:59 -0700

    Displaying data in a view with mongo

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
 	stylus = require('stylus'),
 	logger = require('morgan'),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose');
 
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -26,12 +27,27 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public')); //static route handling
 
+mongoose.connect('mongodb://localhost/MEAN');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+	console.log('MEAN db opened')
+});
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+	mongoMessage = messageDoc.message;
+});
+
 app.get('/partials/:partialPath', function(req, res) {
 	res.render('partials/' + req.params.partialPath);
 })
 
 app.get('*', function(req, res) {
-	res.render('index');
+	res.render('index', {
+		mongoMessage: mongoMessage
+	});
 })
 
 var port = 3030;
