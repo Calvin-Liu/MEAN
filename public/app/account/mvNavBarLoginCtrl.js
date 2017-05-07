96484e4c034a128b569f3096e3c920ba513646a4
Calvin <calvinliu411@gmail.com>
2017-05-06 18:10:13 -0700

    Improved the client login code

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,11 +1,12 @@
- angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http) {
+ angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth) {
+	$scope.identity = mvIdentity;
 	$scope.signin = function(username, password) {
-		$http.post('/login', {username:username, password:password}).then(function(response) {
-			if(response.data.success) {
-				console.log('logged in');
+		mvAuth.authenticateUser(username, password).then(function(success) {
+			if(success) {
+				mvNotifier.notify('You have successfully signed in!');
 			} else {
-				console.log('failed to login');
+				mvNotifier.notify('Username/Password combination incorrect');
 			}
-		})
+		});
 	}
-})
+});
