b5339a797ce8e8370eda94759b5fb821af9fb83b
Calvin <calvinliu411@gmail.com>
2017-05-13 00:49:05 -0700

    Implemented sign up.p. Had to replace vendor folder

--- public/app/app.js
+++ public/app/app.js
@@ -1,24 +1,28 @@
 angular.module('app', ['ngResource', 'ngRoute']);
 
 angular.module('app').config(function($routeProvider, $locationProvider) {
-    var routeRoleChecks = {
-    	admin: {auth: function(mvAuth) {
-			return mvAuth.authorizeCurrentUserForRoute('admin')
-		}}
-    }
+  var routeRoleChecks = {
+    admin: {auth: function(mvAuth) {
+      return mvAuth.authorizeCurrentUserForRoute('admin')
+    }}
+  }
 
-    $locationProvider.html5Mode(true);
-    $routeProvider
-        .when('/', { templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
-        .when('/admin/users', { templateUrl: '/partials/admin/user-list', 
-        		controller: 'mvUserListCtrl', resolve: routeRoleChecks.admin
-        });
+  $locationProvider.html5Mode(true);
+  $routeProvider
+      .when('/', { templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
+      .when('/admin/users', { templateUrl: '/partials/admin/user-list',
+        controller: 'mvUserListCtrl', resolve: routeRoleChecks.admin
+      })
+      .when('/signup', { templateUrl: '/partials/account/signup',
+        controller: 'mvSignupCtrl'
+      })
+
 });
 
 angular.module('app').run(function($rootScope, $location) {
-	$rootScope.$on('$routeChangeError', function(evt, current, previous, rejection) {
-		if(rejection === 'not authorized') {
-			$location.path('/');
-		}
-	})
-});
+  $rootScope.$on('$routeChangeError', function(evt, current, previous, rejection) {
+    if(rejection === 'not authorized') {
+      $location.path('/');
+    }
+  })
+})
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,5 +1,5 @@
-var mongoose = require('mongoose')
-	crypto = require('crypto');
+var mongoose = require('mongoose'),
+	encrypt = require('../utilities/encryption');
 
 module.exports = function(config) {
 	mongoose.connect(config.db);
@@ -19,7 +19,7 @@ module.exports = function(config) {
 	});
 	userSchema.methods = {
 		authenticate: function(passwordToMatch) {
-			return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+			return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
 		}
 	}
 	var User = mongoose.model('User', userSchema);
@@ -27,27 +27,15 @@ module.exports = function(config) {
 	User.find({}).exec(function(err, collection) {
 		if(collection.length == 0) {
 			var salt, hash;
-			salt = createSalt();
-			hash = hashPwd(salt, 'calvin');
+			salt = encrypt.createSalt();
+			hash = encrypt.hashPwd(salt, 'calvin');
 			User.create({firstName:'Calvin', lastName: 'Liu', username:'calvin', salt: salt, hashed_pwd: hash, roles: ['admin']});
-			salt = createSalt();
-			hash = hashPwd(salt, 'john');
+			salt = encrypt.createSalt();
+			hash = encrypt.hashPwd(salt, 'john');
 			User.create({firstName:'John', lastName: 'Papa', username:'john', salt: salt, hashed_pwd: hash, roles: []});
-			salt = createSalt();
-			hash = hashPwd(salt, 'dan');
+			salt = encrypt.createSalt();
+			hash = encrypt.hashPwd(salt, 'dan');
 			User.create({firstName:'Dan', lastName: 'Wahlin', username:'dan', salt: salt, hashed_pwd: hash});
 		}  
 	})
-}
-
-function createSalt() {
-	return crypto.randomBytes(128).toString('base64');
-}
-
-function hashPwd(salt, pwd) {
-	var hmac = crypto.createHmac('sha1', salt);
-	hmac.setEncoding('hex');
-	hmac.write(pwd);
-	hmac.end();
-	return hmac.read();
 }
