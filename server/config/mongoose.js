82e49092ed79f8d4bdef0a42f76511a9f65f08e9
Calvin <calvinliu411@gmail.com>
2017-05-16 21:17:33 -0700

    Caching

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose'),
-	userModel = require('../models/User');
+	userModel = require('../models/User'),
+	courseModel = require('../models/Course');
 
 module.exports = function(config) {
 	mongoose.connect(config.db);
@@ -10,4 +11,5 @@ module.exports = function(config) {
 	});
 
 	userModel.createDefaultUsers();
+	courseModel.createDefaultCourses(); 
 };
